ad617680db567ea34447510011145a1f3d2793f0
David Benjamin <davidben@mit.edu>
2011-02-26 03:26:22 -0500

    Fedora patch: add a NULL check

--- src/npw-common.h
+++ src/npw-common.h
@@ -115,7 +115,7 @@ npw_plugin_instance_is_valid(void *ptr)
 static inline NPW_PluginInstance *
 _npw_get_plugin_instance (NPP instance)
 {
-  return (NPW_PluginInstance *)instance->_NPW_INSTANCE_PRIVATE_DATA;
+  return instance ? (NPW_PluginInstance *)instance->_NPW_INSTANCE_PRIVATE_DATA : NULL;
 }
 
 static inline NPW_PluginInstance *
